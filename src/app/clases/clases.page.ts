d860c04859eb1650acf6b1eecd2b7b131c0306a2
maxramirezg <129181148+maxramirezg@users.noreply.github.com>
2023-09-06 22:54:09 -0300

    Clases

--- src/app/clases/clases.page.ts
+++ src/app/clases/clases.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl,FormGroup,Validators   } from '@angular/forms';
+import { Router,NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-clases',
@@ -7,7 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClasesPage implements OnInit {
 
-  constructor() { }
+  clase1="Clase de programacion PGY3679";
+  clase2="Clase de base de datos BSD8921";
+  clase3="Clase de Arquitectura ASY7803";
+  clase4="Clase de Ingles INI6772";
+
+  clases = new FormGroup({
+    clases1: new FormControl(''),
+    clases2: new FormControl(''),
+    clases3: new FormControl(''),
+    clases4: new FormControl('')
+  });
+  
+  goToPagina1() {
+    console.log(this.clase1)
+    let navigationExtras: NavigationExtras = {
+      state: {clases1: this.clase1}
+      };
+      this.router.navigate(['/generar-qr'],navigationExtras); // Esta linea es la que me permite navegar a otro page 
+  }
+  
+  
+  
+  
+  constructor(private router: Router) { }
+
 
   ngOnInit() {
   }
